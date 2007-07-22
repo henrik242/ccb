a7229b694248c61b18ae8c17a274814a6fd6bfe1
Alexander Jandt <info@squart.de>
2007-07-22 13:02:37 +0200

    Version 1.1 of Carbon CapsBeeper by Alexander Jandt <info@squart.de>
    Fetched from http://www.squart.de/software/ccb/ (2007-07-22)
    
    Carbon CapsBeeper is Public Domain. If you are new to Cocoa you can see how to use Carbon from within a Cocoa App.

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Carbon CapsBeeper
+//
+//  Created by Alex on 13.09.04.
+//  Copyright __MyCompanyName__ 2004. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
